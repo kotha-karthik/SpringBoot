255fbce7db2c3ca2d5c2a80cc5d243eb7874d0e1
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2026-01-06 12:17:08 +0530

    Add RestTemplate and WebClient configuration

---
+++ MicroServices/01)Introduction/MicroServicesDemo/StudentAPi/com.iss.StudentApi/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package com.example.studentapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    public WebClient.Builder webClient() {
+        return WebClient.builder().baseUrl("http://localhost:8080/");
+    }
+}
