a9014bc7198484a3139ac9788b9e38edbf01fec1
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2026-01-06 12:15:29 +0530

    Create Marks.java

---
+++ MicroServices/01)Introduction/MicroServicesDemo/StudentAPi/com.iss.StudentApi/Models/Marks.java
@@ -0,0 +1,20 @@
+package com.example.studentapi.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Marks {
+    private int roll_no;
+    private int math;
+    private int physics;
+    private int chemistry;
+
+}
+
+
