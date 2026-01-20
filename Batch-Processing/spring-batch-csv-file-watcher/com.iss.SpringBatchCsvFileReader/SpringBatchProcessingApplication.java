3a1136f0702ee588272560968a93022b579ecb83
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2026-01-20 17:20:07 +0530

    Create SpringBatchProcessingApplication class

---
+++ Batch-Processing/spring-batch-csv-file-watcher/com.iss.SpringBatchCsvFileReader/SpringBatchProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.iss.springbatchprocessing;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchProcessingApplication.class, args);
+    }
+
+}
